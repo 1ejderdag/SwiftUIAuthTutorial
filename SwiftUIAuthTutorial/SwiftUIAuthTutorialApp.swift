0fe5f91900841bc5823a2e62e00fbbe5f14b69ae
Ejder Dag <ejdrdag@gmail.com>
2024-03-25 14:20:36 +0300

    Initial Commit

---
+++ SwiftUIAuthTutorial/SwiftUIAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAuthTutorialApp.swift
+//  SwiftUIAuthTutorial
+//
+//  Created by Ejder Dağ on 25.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
